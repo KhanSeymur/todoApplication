ab8471bb84b143485e1f792d192b871b362e51cc
Seymur <seymurkhanahmadli@gmail.com>
2022-02-07 19:11:14 +0400

    todo app yarimciq qaldi

---
+++ todo.js
@@ -0,0 +1,12 @@
+const  yeniVezife=document.querySelector('.input-vezife');
+const yeniVezifeElaveBtn=document.querySelector('.btn-vezife-elaveet');
+const vezifeSiyahisi=document.querySelector('.vezife-siyahisi');
+
+
+
+
+//addEventListener
+
+
+yeniVezifeElaveBtn.addEventListener('click',vezifeElaveEt);
+vezifeSiyahisi.addEventListener('click',vezifeTamamlaSil);
